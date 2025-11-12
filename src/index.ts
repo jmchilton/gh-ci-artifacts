a399d35b52589850e0adc35ce409b68e0d0e6b92
John Chilton <jmchilton@gmail.com>
2025-11-12 16:59:32 -0500

    feat(api): export downloadArtifacts for programmatic API use

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { configSchema } from "./config-schema.js";
 export { validateGhSetup } from "./utils/gh.js";
 export { Logger } from "./utils/logger.js";
 export { generateHtmlViewer } from "./html-viewer/index.js";
+export { downloadArtifacts } from "./downloader.js";
